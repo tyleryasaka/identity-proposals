8b3f194dbb5b7ddb2102eddcc90ba84519539ead
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-04 15:56:46 -0700

    Test that ERC725 identity can integrate with identity manager

--- test/identity.js
+++ test/identity.js
@@ -1,6 +1,9 @@
 var Identity = artifacts.require('Identity')
 var Counter = artifacts.require('Counter')
+var IdentityManager = artifacts.require('IdentityManager')
 var Web3 = require('web3')
+
+const web3 = new Web3()
 
 const getEncodedCall = (web3, instance, method, params = []) => {
   const contract = new web3.eth.Contract(instance.abi)
@@ -9,17 +12,43 @@ const getEncodedCall = (web3, instance, method, params = []) => {
 
 contract('Identity', function(accounts) {
   it('should allow the owner to call execute', async function() {
-    const web3 = new Web3()
-    const identity = await Identity.deployed()
-    const counter = await Counter.deployed()
+    // Deploy contracts
+    const identity = await Identity.new()
+    const counter = await Counter.new()
+
+    // Counter should be 0 initially
     assert.equal((await counter.get()).toString(), '0')
+
+    // Call counter.increment from identity
     const encodedCall = getEncodedCall(web3, counter, 'increment')
     const result = await identity.execute(counter.address, 0, encodedCall, { from: accounts[0] })
+
+    // Check that increment was called
     assert.equal((await counter.get()).toString(), '1')
     assert.equal(result.logs.length, 1)
     assert.equal(result.logs[0].event, 'Executed')
     assert.equal(result.logs[0].args.to, counter.address)
     assert.equal(result.logs[0].args.value, 0)
     assert.equal(result.logs[0].args.data, encodedCall)
+  })
+
+  it('should be able to integrate with a key manager', async function() {
+    // Deploy contracts
+    const identity = await Identity.new()
+    const counter = await Counter.new()
+
+    // Counter should be 0 initially
+    assert.equal((await counter.get()).toString(), '0')
+
+    // Transfer identity ownership to the key manager
+    const identityManager = await IdentityManager.new(identity.address, { from: accounts[1] })
+    await identity.transferOwnership(identityManager.address)
+
+    // Call counter.increment from identity, through identity manager
+    const encodedCall = getEncodedCall(web3, counter, 'increment')
+    const result = await identityManager.execute(counter.address, 0, encodedCall, { from: accounts[1] })
+
+    // Check that increment was called
+    assert.equal((await counter.get()).toString(), '1')
   })
 })
