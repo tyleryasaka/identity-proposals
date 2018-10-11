7993923e9111e4857af19f18e63d45b1f5f499e5
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-11 14:00:31 -0700

    Integrate claim verifier with claim manager

--- contracts/ClaimManager.sol
+++ contracts/ClaimManager.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "./ERCXXXX_ClaimManager.sol";
+import "./ERCXXXX_ClaimVerifier.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract ClaimManager is ERCXXXX_ClaimManager, Ownable {
@@ -18,6 +19,7 @@ contract ClaimManager is ERCXXXX_ClaimManager, Ownable {
         bytes32 indexed key,
         uint removedAt
     );
+    ERCXXXX_ClaimVerifier _verifier;
 
     // create or update clams
     function setClaim(address subject, bytes32 key, bytes32 value) public onlyOwner {
@@ -30,11 +32,23 @@ contract ClaimManager is ERCXXXX_ClaimManager, Ownable {
     }
 
     function getClaim(address subject, bytes32 key) public view returns(bytes32) {
+        if (address(_verifier) != 0x0000000000000000000000000000000000000000000000000000000000000000) {
+            bool isValid = _verifier.isClaimValid(address(this), subject, key);
+            if (isValid) {
+                return claims[subject][key];
+            } else {
+                return 0x0000000000000000000000000000000000000000000000000000000000000000;
+            }
+        }
         return claims[subject][key];
     }
 
     function removeClaim(address subject, bytes32 key) public onlyOwner {
         delete claims[subject][key];
         emit ClaimRemoved(subject, key, now);
+    }
+
+    function setVerifier(address verifier) public onlyOwner {
+        _verifier = ERCXXXX_ClaimVerifier(verifier);
     }
 }
