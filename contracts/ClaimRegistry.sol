f621b38182e88e5091ac98f288677dd4eb3c6f52
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-11 00:11:48 -0700

    Add ERC780 interface and implementation

---
+++ contracts/ClaimRegistry.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+import "./ERC780.sol";
+
+/*
+    This code is copied from the ERC780 github issue:
+    https://github.com/uport-project/ethr-did-registry/blob/develop/contracts/EthereumDIDRegistry.sol
+*/
+
+contract ClaimRegistry is ERC780 {
+
+    mapping(address => mapping(address => mapping(bytes32 => bytes32))) public registry;
+
+    event ClaimSet(
+        address indexed issuer,
+        address indexed subject,
+        bytes32 indexed key,
+        bytes32 value,
+        uint updatedAt);
+
+    event ClaimRemoved(
+        address indexed issuer,
+        address indexed subject,
+        bytes32 indexed key,
+        uint removedAt);
+
+    // create or update clams
+    function setClaim(address subject, bytes32 key, bytes32 value) public {
+        registry[msg.sender][subject][key] = value;
+        emit ClaimSet(msg.sender, subject, key, value, now);
+    }
+
+    function setSelfClaim(bytes32 key, bytes32 value) public {
+        setClaim(msg.sender, key, value);
+    }
+
+    function getClaim(address issuer, address subject, bytes32 key) public view returns(bytes32) {
+        return registry[issuer][subject][key];
+    }
+
+    function removeClaim(address issuer, address subject, bytes32 key) public {
+        require(msg.sender == issuer);
+        delete registry[issuer][subject][key];
+        emit ClaimRemoved(msg.sender, subject, key, now);
+    }
+}
---
+++ contracts/ERC780.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+contract ERC780 {
+    function setClaim(address subject, bytes32 key, bytes32 value) public;
+    function setSelfClaim(bytes32 key, bytes32 value) public;
+    function getClaim(address issuer, address subject, bytes32 key) public constant returns(bytes32);
+    function removeClaim(address issuer, address subject, bytes32 key) public;
+}
