d27fa4744f51aab722156394fb20c95ac3d81aef
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-11 14:04:10 -0700

    Remove transferOwnership from ERC725 interface
    I think this is an implementation detail that need not be in the standard

--- contracts/ERC725.sol
+++ contracts/ERC725.sol
@@ -4,8 +4,8 @@ contract ERC725 {
     event Executed(address indexed to, uint256 indexed value, bytes data);
     event ExecutionFailed(address indexed to, uint256 indexed value, bytes data);
     event PointerSet(uint256 indexed key, address indexed value);
+
     function execute(address _to, uint256 _value, bytes _data) public returns (bool _success);
-    function transferOwnership(address newOwner) public;
     function setPointer(uint256 key, address value) public;
     function getPointer(uint256 key) public view returns (address value);
 }
