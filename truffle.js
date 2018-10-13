ff695fefec77036c1b6b7c0bb0f0e9ba2a6620c4
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-12 23:31:03 -0700

    Set up eth-gas-reporter

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,7 +12,13 @@
  *   },
  */
 
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+ module.exports = {
+   // See <http://truffleframework.com/docs/advanced/configuration>
+   // to customize your Truffle configuration!
+   mocha: {
+     reporter: 'eth-gas-reporter',
+     reporterOptions : {
+       currency: 'USD'
+     }
+   }
+ };
