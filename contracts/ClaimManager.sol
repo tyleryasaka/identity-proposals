434ad441b6ab68a0f9042631e42f0b1586f8819b
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-12 21:21:32 -0700

    Remove the claim verifier interface
    Just realized this is an implementation detail. Because each issuer can deploy their own implementation, they can implement computable claims however they want. No need for a standard interface to check claim validity.
    
    Guess that's a nice benefit of this architecture.

--- contracts/ClaimManager.sol
+++ contracts/ClaimManager.sol
@@ -1,12 +1,10 @@
 pragma solidity ^0.4.24;
 
 import "./ERCXXXX_ClaimManager.sol";
-import "./ERCXXXX_ClaimVerifier.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract ClaimManager is ERCXXXX_ClaimManager, Ownable {
     mapping(address => mapping(bytes32 => bytes32)) public claims;
-    ERCXXXX_ClaimVerifier _verifier;
 
     // create or update clams
     function setClaim(address subject, bytes32 key, bytes32 value) public onlyOwner {
@@ -19,23 +17,11 @@ contract ClaimManager is ERCXXXX_ClaimManager, Ownable {
     }
 
     function getClaim(address subject, bytes32 key) public view returns(bytes32) {
-        if (address(_verifier) != 0x0000000000000000000000000000000000000000000000000000000000000000) {
-            bool isValid = _verifier.isClaimValid(address(this), subject, key);
-            if (isValid) {
-                return claims[subject][key];
-            } else {
-                return 0x0000000000000000000000000000000000000000000000000000000000000000;
-            }
-        }
         return claims[subject][key];
     }
 
     function removeClaim(address subject, bytes32 key) public onlyOwner {
         delete claims[subject][key];
         emit ClaimRemoved(subject, key, now);
-    }
-
-    function setVerifier(address verifier) public onlyOwner {
-        _verifier = ERCXXXX_ClaimVerifier(verifier);
     }
 }
--- test/claimManager.js
+++ test/claimManager.js
@@ -1,5 +1,4 @@
 var ClaimManager = artifacts.require('ClaimManager')
-var ClaimVerifier = artifacts.require('ClaimVerifier')
 
 const claimKey = '0x0000000000000000000000000000000000000000000000000000000000000000'
 const claimValue = '0x0000000000000000000000000000000000000000000000000000000000000123'
@@ -23,29 +22,5 @@ contract('ClaimManager', function(accounts) {
     // Chefck that claim was removed
     const removedClaim = await claimManager.getClaim(accounts[1], claimKey)
     assert.equal(removedClaim, emptyClaim)
-  })
-
-  it('should be able to integrate with claim verifier', async function() {
-    // Deploy contracts
-    const claimManager = await ClaimManager.new()
-    const claimVerifier = await ClaimVerifier.new()
-
-    // Set up claim verifier
-    await claimVerifier.setRegistrationStatus(true)
-    claimManager.setVerifier(claimVerifier.address)
-
-    // Call setClaim
-    await claimManager.setClaim(accounts[1], claimKey, claimValue)
-
-    // Call getClaim
-    const claim = await claimManager.getClaim(accounts[1], claimKey)
-    assert.equal(claim, claimValue)
-
-    // Set registration status to closed
-    await claimVerifier.setRegistrationStatus(false)
-
-    // Chefck that claim is no longer valid
-    const invalidatedClaim = await claimManager.getClaim(accounts[1], claimKey)
-    assert.equal(invalidatedClaim, emptyClaim)
   })
 })
