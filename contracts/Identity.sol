0ca01a5a7f6d5110a32c9fe7483861f6c8b8644b
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-13 00:11:45 -0700

    Implement light version of Ownable
    Demonstrates how gas costs can be further optimized

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -5,8 +5,23 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /* TODO: implement ERC165 */
 
-contract Identity is ERC725, Ownable {
+contract Identity is ERC725 {
+    address private _owner;
     mapping(uint256 => address) ambassadors;
+
+    constructor() public {
+      _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == _owner);
+      _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        _owner = newOwner;
+    }
 
     function execute(address _to, uint256 _value, bytes _data) public onlyOwner returns (bool _success) {
         _success = _to.call(_data, _value);
