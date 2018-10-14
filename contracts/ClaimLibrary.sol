55a784a62590effe087540f1b10714fed4c07cbc
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-14 15:16:59 -0700

    Fix misnamed param

--- contracts/ClaimLibrary.sol
+++ contracts/ClaimLibrary.sol
@@ -6,10 +6,10 @@ import "./ERCXXXX_ClaimManager.sol";
 library ClaimLibrary {
     bytes32 constant CLAIM_MANAGEMENT_DELEGATE_TYPE = 0x0000000000000000000000000000000000000000000000000000000000000123;
 
-    function getClaim(address issuer, address subject, bytes32 delegateType) public view returns(bytes32) {
+    function getClaim(address issuer, address subject, bytes32 key) public view returns(bytes32) {
         ERCXXXX_Identity identity = ERCXXXX_Identity(issuer);
         address claimManagerAddress = identity.getDelegate(CLAIM_MANAGEMENT_DELEGATE_TYPE);
         ERCXXXX_ClaimManager claimManager = ERCXXXX_ClaimManager(claimManagerAddress);
-        return claimManager.getClaim(subject, delegateType);
+        return claimManager.getClaim(subject, key);
     }
 }
