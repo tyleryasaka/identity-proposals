cff881dddeb648bfd8aca82fa8bde78a5767d257
Tyler Yasaka <contact@tyleryasaka.me>
2018-10-15 21:09:46 -0700

    Rename standard

--- contracts/ClaimIssuer.sol
+++ contracts/ClaimIssuer.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "./ERCXXXX_ClaimIssuerRegistry.sol";
+import "./ERCXXXX_ClaimIssuer.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract ClaimIssuer is ERCXXXX_ClaimIssuer, Ownable {
