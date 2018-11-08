f45785b9c43d4c9d5e7d6b1c87bd73c1bff3a8d7
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-07 23:12:43 -0800

    Allow creating identity from demo UI

--- claims-library-ethereum/demo/index.js
+++ claims-library-ethereum/demo/index.js
@@ -39,25 +39,31 @@ function mainView (state, emit) {
       <header class="tc pv4 pv5-ns">
         <img src=${src} class="br-100 pa1 ba b--black-10 h3 w3" alt="avatar">
         <h1 class="f5 f4-ns fw6 mid-gray">${_name}</h1>
-        <h2 class="f6 gray fw2 tracked">${did}</h2>
+        ${did ? html`
+          <h2 class="f6 gray fw2 tracked">${did}</h2>
+        ` : html `
+          <button class="b ph3 pv2 ba b--black bg-transparent grow pointer f6" onclick=${createIdentity}>Create identity</button>
+        `}
       </header>
-      <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
-        <article class="pa4 black-80">
-          <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-            <legend class="ph0 mh0 fw6 clip">Sign Up</legend>
-            <div class="mt3">
-              <label class="db fw4 lh-copy f6" for="spirit-animal">Spirit Animal</label>
-              <input class="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="spirit-animal" onchange=${updateSpiritAnimal} value=${inputSpiritAnimalValue} ${spiritAnimalDisabled}>
-            </div>
-          </fieldset>
-          ${!hasSpiritAnimal ? html`
-            <div class="mt3"><button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onclick=${addSpiritAnimal}>Claim my spirit animal</button></div>
-          ` : null}
-          ${hasSpiritAnimal ? html`
-            <div class="mt3"><button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onclick=${removeSpiritAnimal}>Remove this claim</button></div>
-          ` : null}
-        </article>
-      </section>
+      ${did ? html`
+        <section class="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+          <article class="pa4 black-80">
+            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+              <legend class="ph0 mh0 fw6 clip">Sign Up</legend>
+              <div class="mt3">
+                <label class="db fw4 lh-copy f6" for="spirit-animal">Spirit Animal</label>
+                <input class="pa2 input-reset ba bg-transparent w-100 measure" type="text" name="spirit-animal" onchange=${updateSpiritAnimal} value=${inputSpiritAnimalValue} ${spiritAnimalDisabled}>
+              </div>
+            </fieldset>
+            ${!hasSpiritAnimal ? html`
+              <div class="mt3"><button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onclick=${addSpiritAnimal}>Claim my spirit animal</button></div>
+            ` : null}
+            ${hasSpiritAnimal ? html`
+              <div class="mt3"><button class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onclick=${removeSpiritAnimal}>Remove this claim</button></div>
+            ` : null}
+          </article>
+        </section>
+      ` : null }
     </body>
   `
   return unlocked ? content : lockMessage
@@ -81,7 +87,12 @@ function mainView (state, emit) {
 
   async function removeSpiritAnimal() {
     await claimtasticEthereum.removeClaim(spiritAnimalClaim.selfClaim.id)
+    emit('updateSpiritAnimal', '')
     emit('loadClaims')
+  }
+
+  async function createIdentity() {
+    emit('createIdentity')
   }
 }
 
@@ -103,6 +114,12 @@ function store (state, emitter) {
 
   emitter.on('updateSpiritAnimal', async function(inputSpiritAnimal) {
     state.inputSpiritAnimal = inputSpiritAnimal
+    emitter.emit('render')
+  })
+
+  emitter.on('createIdentity', async function () {
+    const _did = await state.claimtasticEthereum.createIdentity()
+    state.did = _did
     emitter.emit('render')
   })
 
