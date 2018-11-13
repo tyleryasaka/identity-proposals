c333ee8a612239720ec928cb32269a947413dbff
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-12 18:07:26 -0800

    build for  heroku

--- contracts/ERC780.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract ERC780 {
-    function setClaim(address subject, bytes32 key, bytes32 value) public;
-    function setSelfClaim(bytes32 key, bytes32 value) public;
-    function getClaim(address issuer, address subject, bytes32 key) public constant returns(bytes32);
-    function removeClaim(address issuer, address subject, bytes32 key) public;
-}
